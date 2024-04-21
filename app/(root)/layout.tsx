9a5114d180d270b8316a68933c702c9e3f54955b
unknown <7.thuydung.2001@gmail.com>
2024-04-21 23:59:40 +0700

    Navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "../globals.css";
 
 import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "../components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={inter.className}>
-                <ClerkProvider>{children}</ClerkProvider>
+                <ClerkProvider> <Navbar/> {children}</ClerkProvider>
             </body>
         </html>
     );
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+"use client"
+import { UserButton } from "@clerk/nextjs";
+import { Home, Menu } from "lucide-react";
+import Link from "next/link";
+import React, { useState } from "react";
 
 const Navbar = () => {
-    return <div></div>;
+    const [dropMenu, setDropMenu] = useState(false)
+    console.log(dropMenu)
+    return (
+        <div className="flex justify-between items-center p-5">
+
+            <Link href="/">
+                <h1 className="text-[50px]">Tour </h1>
+            </Link>
+
+            <ul className="flex justify-center gap-5 max-md:hidden">
+                <Link href="/home">
+                    <li className="flex  justify-center items-center gap-3 text-[20px]" >
+                        <Home />home
+                    </li>
+                </Link>
+
+                <Link href="/tag">
+                    <li className="flex  justify-center items-center gap-3 text-[20px]" >
+                        <Home />tag
+                    </li>
+                </Link>
+
+                <Link href="/name">
+                    <li className="flex  justify-center items-center gap-3 text-[20px]" >
+                        <Home />na
+                    </li>
+                </Link>
+            </ul>
+            <div className="flex items-center justify-center gap-4 border-[1px] p-3 rounded-[40px] shadow-md relative">
+                <UserButton />
+                <Menu onClick={() => setDropMenu(!dropMenu)} />
+                {dropMenu && (
+                    <ul className=" absolute top-[60px] left-[-30px] shadow-md bg-white p-5 border-[1px] rounded-lg flex flex-col items-start justify-start gap-3">
+                        <Link href="/home">
+                            <li className="flex  justify-center items-center gap-3 text-[20px]" >
+                                <Home />home
+                            </li>
+                        </Link>
+
+                        <Link href="/tag">
+                            <li className="flex  justify-center items-center gap-3 text-[20px]" >
+                                <Home />tag
+                            </li>
+                        </Link>
+
+                        <Link href="/name">
+                            <li className="flex  justify-center items-center gap-3 text-[20px]" >
+                                <Home />na
+                            </li>
+                        </Link>
+                    </ul>)}
+            </div>
+
+        </div>);
 };
 
 export default Navbar;
